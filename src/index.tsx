e6a9448b8a7836ad3cb6cd9d099f038ec1815a23
hezhisheng <zhishenghe@aaxischina.com>
2022-10-21 15:22:20 +0800

    optimization entry file

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,5 @@
 import './style/index.less';
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-
 import RouterWrapper from './Router';
 
 const root = ReactDOM.createRoot(document.getElementById('app'));
