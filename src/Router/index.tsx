88e4c106c270c7de10d28c35fabbe5ee0b30e5fc
hezhisheng <zhishenghe@aaxischina.com>
2022-11-21 15:23:31 +0800

    edit router message

--- src/Router/index.tsx
+++ src/Router/index.tsx
@@ -23,18 +23,3 @@ const routes = [
 ]
 
 export default routes;
-
-
-
-
-// const router: React.ReactNode = <BrowserRouter>
-//   <Routes>
-//     <Route path="*" element={<App />}>
-//       <Route path="*/home" element={<Home />} />
-//       <Route path="*/about" element={<About />} />
-//     </Route>
-//   </Routes>
-
-// </BrowserRouter>
-
-// export default router
