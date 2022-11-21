ffacd095e24e34220ed69396305f24d145430234
hezhisheng <zhishenghe@aaxischina.com>
2022-11-21 15:20:43 +0800

    add router

--- config/plugins/resource-analysis-webpack-plugin.js
+++ config/plugins/resource-analysis-webpack-plugin.js
@@ -8,10 +8,18 @@ class ResourceAnalysisWebpackPlugin {
 
         const context = compiler.context,
             fs = compiler.outputFileSystem;
+            
+            compiler.hooks.run.tap('pluginName', (compilation) => {
+                console.log(compilation, 'webpack 构建过程开始！');
+              });
+
+        compiler.hooks.make.tap('ResourceAnalysisWebpackPlugin', compilation => {
+            // console.log(compilation, 'compilation');
+        })
 
         compiler.hooks.emit.tap('ResourceAnalysisWebpackPlugin', compilation => {
 
-            const assets = Object.entries(compilation.assets);
+            const assets = Object.entries(compilation.assets);  // 资源转数组
 
             let fileContent = `[index]  fileName >>> fileSize\n`,
                 TotalSize = 0;
@@ -20,20 +28,20 @@ class ResourceAnalysisWebpackPlugin {
 
                 const value = (item[1].size() / 1024).toFixed(2);
 
-                TotalSize += +value;
+                TotalSize += +value;  // 累加每项资源大小
                 fileContent += `\n[${idx + 1}]  ${item[0]} >>> ${value}KB`;
 
-                if (idx + 1 === assets.length) {
+                if (idx + 1 === assets.length) { //如果索引为数组长度，则输出资源总大小
 
                     fileContent += `\n\ntotalSize >>> ${(TotalSize / 1024).toFixed(2)}MB`;
                 }
 
             })
 
-            if (fs.existsSync(`${context}/resource-analysis-file`)) {
+            if (fs.existsSync(`${context}/resource-analysis-file`)) {  
                 this.appendFile(fs, context, fileContent)
             } else {
-                fs.mkdirSync(`${context}/resource-analysis-file`)
+                fs.mkdirSync(`${context}/resource-analysis-file`)  //没有目录则创建
                 this.appendFile(fs, context, fileContent)
             }
         })
